c15df616e6a3dac45592af3509acf7206246d5b1
mosh <mosh.ohayo@gmail.com>
2019-12-11 21:02:17 +0900

    Fixed benchmark
    Now returns entire vector in benchmark lambda

--- benches/bool.rs
+++ benches/bool.rs
@@ -8,7 +8,7 @@ use specsort::SpecSort;
 use test::Bencher;
 
 fn sort_bool(s: &mut [bool]) {
-    if cfg!(feature="bench_std") {
+    if cfg!(feature = "bench_std") {
         s.sort_unstable()
     } else {
         <bool as SpecSort>::sort_unstable(s);
@@ -16,38 +16,34 @@ fn sort_bool(s: &mut [bool]) {
 }
 
 fn bench_random(b: &mut Bencher, size: usize) {
-    unsafe {
-        let mut v = Vec::with_capacity(size);
-        for _ in 0..size {
-            v.push(rand::random());
-        }
-
-        b.iter(|| {
-            let mut v = v.clone();
-            sort_bool(&mut v);
-            *v.get_unchecked(0)
-        })
+    let mut v = Vec::with_capacity(size);
+    for _ in 0..size {
+        v.push(rand::random());
     }
+
+    b.iter(|| {
+        let mut v = v.clone();
+        sort_bool(&mut v);
+        v
+    })
 }
 
 fn bench_sorted(b: &mut Bencher, size: usize) {
-    unsafe {
-        let mut v: Vec<bool> = Vec::with_capacity(size);
-        let mid = thread_rng().gen_range(0, size);
-        let mut v = Vec::with_capacity(size);
-        for _ in 0..mid {
-            v.push(false);
-        }
-        for _ in mid..size {
-            v.push(true);
-        }
-
-        b.iter(|| {
-            let mut v = v.clone();
-            sort_bool(&mut v);
-            *v.get_unchecked(0)
-        })
+    let mut v: Vec<bool> = Vec::with_capacity(size);
+    let mid = thread_rng().gen_range(0, size);
+    let mut v = Vec::with_capacity(size);
+    for _ in 0..mid {
+        v.push(false);
     }
+    for _ in mid..size {
+        v.push(true);
+    }
+
+    b.iter(|| {
+        let mut v = v.clone();
+        sort_bool(&mut v);
+        v
+    })
 }
 
 #[bench]
